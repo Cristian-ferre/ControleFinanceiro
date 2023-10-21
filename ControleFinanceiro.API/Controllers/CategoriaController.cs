0203eb04344adc8612d7871102500380468971da
Cristian Ferreira <cristianferreiradeoliveira.ti@gmail.com>
2023-10-20 22:27:21 -0300

    wip: metodo post de nova Categoria desenvovido

--- ControleFinanceiro.API/Controllers/CategoriaController.cs
+++ ControleFinanceiro.API/Controllers/CategoriaController.cs
@@ -1,11 +1,12 @@
 ﻿using ControleFinanceiro.Dados.Context;
+using ControleFinanceiro.Dominio.DTOs;
 using ControleFinanceiro.Dominio.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ControleFinanceiro.API.Controllers
 {
-    public class CategoriaController : ControllerBase
+    public class CategoriaController : Controller
     {
 
         private ControleFinanceiroDbContext _context;
@@ -16,18 +17,31 @@ namespace ControleFinanceiro.API.Controllers
         }
 
         [HttpPost("ControleFianceiro/AdicinarNovaCategoria")]
-        public ActionResult PostCategoria([FromBody] Categorias categorias)
+        public ActionResult PostCategoria([FromBody] CategoriaDTO categorias)
         {
-            var Categorias = new Categorias
+            try
             {
-                CategoriaNome = categorias.CategoriaNome,
-                CategoriaDescricao = categorias.CategoriaDescricao,
-            };
+                if (categorias == null)
+                {
+                    return BadRequest("Dados de categoria inválidos");
+                }
 
-            _context.Categorias.Add(Categorias);
-            _context.SaveChanges();
+                var Categorias = new Categorias
+                {
+                    CategoriaNome = categorias.CategoriaNome,
+                    CategoriaDescricao = categorias.CategoriaDescricao,
+                };
 
-            return Ok(Categorias);
+                _context.Categorias.Add(Categorias);
+                _context.SaveChanges();
+
+                return Json(new { success = true, message = $"{Categorias.CategoriaNome} Inserido com sucesso" });
+            }
+            catch
+            {
+                return StatusCode(500, new { success = false, message = "Ocorreu um erro interno no servidor" });
+            }
+
         }
 
     }

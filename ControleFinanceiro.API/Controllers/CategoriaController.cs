5522b32cae96d508a480be5fe3ef6a864eaa8aec
Cristian Ferreira <cristianferreiradeoliveira.ti@gmail.com>
2023-10-20 21:44:14 -0300

    wip: Controller categoria desenvolvido

---
+++ ControleFinanceiro.API/Controllers/CategoriaController.cs
@@ -0,0 +1,34 @@
+﻿using ControleFinanceiro.Dados.Context;
+using ControleFinanceiro.Dominio.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleFinanceiro.API.Controllers
+{
+    public class CategoriaController : ControllerBase
+    {
+
+        private ControleFinanceiroDbContext _context;
+
+        public CategoriaController(ControleFinanceiroDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("ControleFianceiro/AdicinarNovaCategoria")]
+        public ActionResult PostCategoria([FromBody] Categorias categorias)
+        {
+            var Categorias = new Categorias
+            {
+                CategoriaNome = categorias.CategoriaNome,
+                CategoriaDescricao = categorias.CategoriaDescricao,
+            };
+
+            _context.Categorias.Add(Categorias);
+            _context.SaveChanges();
+
+            return Ok(Categorias);
+        }
+
+    }
+}

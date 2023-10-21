647cc0cdd3880188cff259e08247f96f58b04183
Cristian Ferreira <cristianferreiradeoliveira.ti@gmail.com>
2023-10-20 21:46:52 -0300

    wip: pasta DTO e categoriaDTO desenvolvida

---
+++ ControleFinanceiro.Dominio/DTOs/CategoriaDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleFinanceiro.Dominio.DTOs
+{
+    internal class CategoriaDTO
+    {
+        public string CategoriaNome { get; set; }
+        public string CategoriaDescricao { get; set; }
+    }
+}

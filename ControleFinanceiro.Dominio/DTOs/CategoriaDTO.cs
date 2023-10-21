7a3ac9cbe032dbc03d7786600839f355f01d6e86
Cristian Ferreira <cristianferreiradeoliveira.ti@gmail.com>
2023-10-20 21:47:41 -0300

    wip: CategoriaDTO para public

--- ControleFinanceiro.Dominio/DTOs/CategoriaDTO.cs
+++ ControleFinanceiro.Dominio/DTOs/CategoriaDTO.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace ControleFinanceiro.Dominio.DTOs
 {
-    internal class CategoriaDTO
+    public class CategoriaDTO
     {
         public string CategoriaNome { get; set; }
         public string CategoriaDescricao { get; set; }

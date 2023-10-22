a4c27888ae97ceb9c99a156449123c75a83550e7
Cristian Ferreira <cristianferreiradeoliveira.ti@gmail.com>
2023-10-22 15:03:08 -0300

    wip: metodo post de nova receita desenvolvido

--- ControleFinanceiro.API/Controllers/ReceitaController.cs
+++ ControleFinanceiro.API/Controllers/ReceitaController.cs
@@ -1,4 +1,6 @@
 ﻿using ControleFinanceiro.Dados.Context;
+using ControleFinanceiro.Dominio.DTOs;
+using ControleFinanceiro.Dominio.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ControleFinanceiro.API.Controllers
@@ -13,6 +15,32 @@ namespace ControleFinanceiro.API.Controllers
             _context = context;
         }
 
+        [HttpPost("ControleFianceiro/AdicinarNovaReceita")]
+        public ActionResult postReceita([FromBody] ReceitaDTO receitas)
+        {
+            try
+            {
+                var Receitas = new Receitas
+                {
+                    TipoValor = receitas.TipoValor,
+                    ReceitaName = receitas.ReceitaName,
+                    ReceitaDescricao = receitas.ReceitaDescricao,
+                    ReceitaValor = receitas.ReceitaValor,
+                    ReceitaData = receitas.ReceitaData,
+                    ReceitaQuantidadeMeses = receitas.ReceitaQuantidadeMeses,
+                    UsuarioId = receitas.UsuarioId,
+                };
 
+                _context.Receitas.Add(Receitas);
+                _context.SaveChanges();
+
+                return Json(new { success = true, message = $"{Receitas.ReceitaName} Inserido com sucesso" });
+            }
+            catch
+            {
+                return StatusCode(500, new { success = false, message = "Ocorreu um erro interno no servidor" });
+
+            }
+        }
     }
 }

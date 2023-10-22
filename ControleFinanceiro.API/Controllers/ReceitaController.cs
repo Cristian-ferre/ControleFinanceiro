34edc02b8ec9dd66ea148ea281ff5f2e6805aa33
Cristian Ferreira <cristianferreiradeoliveira.ti@gmail.com>
2023-10-22 12:51:23 -0300

    wip: controller receita criada e estruturada

---
+++ ControleFinanceiro.API/Controllers/ReceitaController.cs
@@ -0,0 +1,18 @@
+﻿using ControleFinanceiro.Dados.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleFinanceiro.API.Controllers
+{
+    public class ReceitaController : Controller
+    {
+
+        private ControleFinanceiroDbContext _context;
+
+        public ReceitaController(ControleFinanceiroDbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}

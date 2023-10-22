9026200f1314181b14df73e549d53a7bf8bb0ec3
Cristian Ferreira <cristianferreiradeoliveira.ti@gmail.com>
2023-10-22 14:37:42 -0300

    wip: receitaDTO criada

---
+++ ControleFinanceiro.Dominio/DTOs/ReceitaDTO.cs
@@ -0,0 +1,23 @@
+﻿using ControleFinanceiro.Dominio.Entities;
+using ControleFinanceiro.Dominio.Enums;
+
+namespace ControleFinanceiro.Dominio.DTOs
+{
+    public class ReceitaDTO
+    {
+        public string ReceitaName { get; set; }
+
+        public string ReceitaDescricao { get; set; }
+
+
+        public double ReceitaValor { get; set; }
+
+        public DateTime ReceitaData { get; set; }
+
+        public int ReceitaQuantidadeMeses { get; set; }
+
+        public TipoValor TipoValor { get; set; }
+
+        public int UsuarioId { get; set; }
+    }
+}

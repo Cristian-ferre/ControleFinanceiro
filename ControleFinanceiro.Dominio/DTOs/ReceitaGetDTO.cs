f783045c8accecdae93731ad61cbbd97c0266b61
Cristian Ferreira <cristianferreiradeoliveira.ti@gmail.com>
2023-11-04 20:28:17 -0300

    wip: ReceitaGetDTO estruturada para exibir uma listas de receitas

---
+++ ControleFinanceiro.Dominio/DTOs/ReceitaGetDTO.cs
@@ -0,0 +1,24 @@
+﻿using ControleFinanceiro.Dominio.Entities;
+using ControleFinanceiro.Dominio.Enums;
+
+namespace ControleFinanceiro.Dominio.DTOs
+{
+    public class ReceitaGetDTO
+    {
+        public int ReceitaId { get; set; }
+
+        public string ReceitaName { get; set; }
+
+        public string ReceitaDescricao { get; set; }
+
+
+        public double ReceitaValor { get; set; }
+
+        public DateTime ReceitaData { get; set; }
+
+        public int ReceitaQuantidadeMeses { get; set; }
+
+        public TipoValor TipoValor { get; set; }
+
+    }
+}

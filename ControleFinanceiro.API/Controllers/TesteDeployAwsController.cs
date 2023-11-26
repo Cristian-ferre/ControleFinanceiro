08b5dfb528bfa4ea4776e3b74a322db300d0b68c
Cristian Ferreira <cristianferreiradeoliveira.ti@gmail.com>
2023-11-25 21:50:54 -0300

    feat: controller teste sem acesso ao banco implementada

---
+++ ControleFinanceiro.API/Controllers/TesteDeployAwsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleFinanceiro.API.Controllers
+{
+    [Route("ControleFinanceiro/TesteDeployAws/")]
+    public class TesteDeployAwsController : Controller
+    {
+        [HttpGet("SemBanco")]
+        public IActionResult SemBanco()
+        {
+
+            return Ok("Hello Word!!");
+        }
+    }
+}

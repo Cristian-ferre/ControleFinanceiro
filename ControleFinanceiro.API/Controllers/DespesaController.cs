7507aea686d3b03c9ab7ed15003d21cad4fd5f62
Cristian Ferreira <cristianferreiradeoliveira.ti@gmail.com>
2023-11-26 17:48:51 -0300

    wip: controller estruturada e metodo adicionar iniciado

--- ControleFinanceiro.API/Controllers/DespesaController.cs
+++ ControleFinanceiro.API/Controllers/DespesaController.cs
@@ -1,6 +1,24 @@
-﻿namespace ControleFinanceiro.API.Controllers
+﻿using ControleFinanceiro.Dados.Repositories;
+using ControleFinanceiro.Dominio.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleFinanceiro.API.Controllers
 {
-    public class DespesaController
+    [Route("/ControleFinanceiro/Despesa/")]
+    public class DespesaController : Controller
     {
+        private readonly RepositoryDespesa _repositoryDespesa;
+
+        public DespesaController(RepositoryDespesa repositoryDespesa)
+        {
+            _repositoryDespesa = repositoryDespesa;
+        }
+
+        [HttpPost("Adicionar")]
+        public ActionResult Adicionar([FromBody] DespesaDTO despesa)
+        {
+            var result = _repositoryDespesa.Adicionar(despesa);
+            return Ok(result);
+        }
     }
 }

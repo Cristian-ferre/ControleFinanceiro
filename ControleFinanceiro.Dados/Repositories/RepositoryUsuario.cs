5ee05afae69500cb6453221fdba3a7b90d51a9e4
Cristian Ferreira <cristianferreiradeoliveira.ti@gmail.com>
2024-03-24 11:31:18 -0300

    feat: interface e repository para trabalhar com usuario implementado

---
+++ ControleFinanceiro.Dados/Repositories/RepositoryUsuario.cs
@@ -0,0 +1,30 @@
+﻿using ControleFinanceiro.Dados.Context;
+using ControleFinanceiro.Dominio.Entities;
+using ControleFinanceiro.Dominio.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ControleFinanceiro.Dados.Repositories
+{
+    public class RepositoryUsuario : IRepositoryUsuario
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ControleFinanceiroDbContext _context;
+
+        public RepositoryUsuario(IConfiguration configuration, ControleFinanceiroDbContext context)
+        {
+            _configuration = configuration;
+            _context = context;
+        }
+
+        public async Task<Usuarios> ObterUsuario(int usuarioID)
+        {
+            return await _context.Usuarios.FirstOrDefaultAsync(u => u.UsuarioId == usuarioID);
+        }
+
+        public async Task<bool> UsuarioExiste(string name, string senha)
+        {
+            return await  _context.Usuarios.AnyAsync(u => u.Name == name || u.Senha == senha);
+        }
+    }
+}
---
+++ ControleFinanceiro.Dominio/Interfaces/IRepositoryUsuario.cs
@@ -0,0 +1,11 @@
+﻿using ControleFinanceiro.Dominio.Entities;
+
+namespace ControleFinanceiro.Dominio.Interfaces
+{
+    public interface IRepositoryUsuario
+    {
+        Task<bool> UsuarioExiste(string name, string senha); 
+
+        Task<Usuarios> ObterUsuario(int usuarioID);
+    }
+}

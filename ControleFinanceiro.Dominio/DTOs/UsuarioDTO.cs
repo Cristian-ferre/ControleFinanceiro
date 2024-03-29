a3966caaba31cd5b8f7631228f1de67550275bd0
Cristian Ferreira <cristianferreiradeoliveira.ti@gmail.com>
2024-03-29 13:11:41 -0300

    chore: alteração e adicionados novos atributos

--- ControleFinanceiro.Dominio/DTOs/UsuarioDTO.cs
+++ ControleFinanceiro.Dominio/DTOs/UsuarioDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,13 @@ namespace ControleFinanceiro.Dominio.DTOs
 {
     public class UsuarioDTO
     {
-        public string Name { get; set; }
+        public required string Nome { get; set; }
 
-        public string Senha { get; set; }
+        public required string Senha { get; set; }
+
+        public required string Email { get; set; }
+
+        public string? Foto { get; set; }
 
     }
 }
